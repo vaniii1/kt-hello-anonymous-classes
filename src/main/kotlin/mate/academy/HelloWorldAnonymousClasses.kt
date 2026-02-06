3729bff011511e9db4e9360093ca85a41a856636
ivanhromovyi <ivanh@uia.no>
2026-02-06 10:49:02 +0100

    implement object expression logic

--- src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
+++ src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
@@ -8,6 +8,45 @@ class HelloWorldAnonymousClasses {
     }
 
     fun sayHello(names: List<String>): List<String> {
-        // TODO: implement
+        var englishGreeting = object : HelloWorldGreeting {
+            override fun greet(): String {
+                return "Hello world"
+            }
+
+            override fun greetSomeone(someone: String): String {
+                return "Hello $someone"
+            }
+        }
+
+        var frenchGreeting = object : HelloWorldGreeting {
+            override fun greet(): String {
+                return "Salut tout le monde"
+            }
+
+            override fun greetSomeone(someone: String): String {
+                return "Salut $someone"
+            }
+        }
+        var spanishGreeting = object : HelloWorldGreeting {
+            override fun greet(): String {
+                return "Hola, mundo"
+            }
+
+            override fun greetSomeone(someone: String): String {
+                return "Hola, $someone"
+            }
+        }
+
+        val list = listOf(englishGreeting, frenchGreeting, spanishGreeting)
+        val result : MutableList<String> = mutableListOf()
+        result.addAll(list.map { it.greet() })
+        for (name in names) {
+            result.addAll(list.map { it.greetSomeone(name) })
+        }
+        return result
     }
+}
+
+fun main() {
+    println(HelloWorldAnonymousClasses().sayHello(listOf("John", "Denise")))
 }
